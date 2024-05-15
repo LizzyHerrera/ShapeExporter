0a7901f6157055ffc7d974a22edcaa4d74fdc051
LizHerreraa <liz@genies.com>
2024-05-15 15:08:50 -0700

    Added assignment folder and files

---
+++ 4_ui/assignment/42_reflect.py
@@ -0,0 +1,39 @@
+#******************************************************************************
+# content = assignment
+#
+# date    = 2024-01-01
+# email   = contact@alexanderrichtertd.com
+#*******************************************************************************
+
+
+
+print("**********************************************************************")
+print("# REFLECTION")
+print("**********************************************************************")
+"""
+Let us look back together and see how far we have come after the last 8 weeks.
+In the beginning we set up the why and specific goals for a clear focus.
+Today we will reflect on them and the process.
+
+Fill out your reflection:
+https://forms.gle/v4tAKqo9kMrLJxfv8
+"""
+
+
+
+print("**********************************************************************")
+print("# SHARING")
+print("**********************************************************************")
+"""
+a) Share your tool with our Discord group at #final-project:
+
+	1. Explain what your tool does and why
+	2. Share images and video links
+
+b) Share your tool on LinkedIn as well and tag us: 
+   www.linkedin.com/in/alexanderrichtertd
+   www.linkedin.com/company/alexanderrichtertd
+
+   This is an opportunity to show your progress in Python
+   with your current and future employers.
+"""
